331c9ce65250ae02e73c72363f286effb135bee0
Tanim1254 <78093039+Tanim1254@users.noreply.github.com>
2022-07-06 14:01:30 +0600

    Add files via upload

---
+++ Controllers/CvController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurriculamVitae.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
